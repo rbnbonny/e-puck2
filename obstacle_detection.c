acada5cfa0cfc8518efa9647e96a562082ff3a18
Robin Bonny <robin.bonny@epfl.ch>
2021-04-19 14:42:43 +0200

    IR Threadding

--- obstacle_detection.c
+++ obstacle_detection.c
@@ -5,29 +5,46 @@
 #include "sensors/proximity.h"
 #include "sensors/VL53L0X/VL53L0X.h"
 
-static thread_t *obstacleThd;
+#define IR_SENSORNUM 0
 
-static THD_FUNCTION(obstacle_detection_thd, arg) {
+static thread_t *TOFthd;
+static thread_t *IRthd;
+
+static THD_FUNCTION(TOFsensor_thd, arg) {
 	(void) arg;
 	chRegSetThreadName(__FUNCTION__);
 
-	chThdSleepMilliseconds(100);
-
 	while (1) {
 		volatile uint16_t testTOF = VL53L0X_get_dist_mm();
-		chprintf((BaseSequentialStream *) &SD3,"TOF Distance: %d mm \r\n", testTOF);
+		chprintf((BaseSequentialStream *) &SD3, "TOF Distance: %d mm \r\n",
+				testTOF);
 		chThdSleepMilliseconds(100);
 	}
 }
 
+static THD_FUNCTION(IRsensor_thd, arg) {
+	(void) arg;
+	chRegSetThreadName(__FUNCTION__);
+
+//	calibrate_ir();
+//	while (1) {
+//		volatile int testIR = get_calibrated_prox(IR_SENSORNUM);
+//		chprintf((BaseSequentialStream *) &SD3, "IR Level: %d \r\n", testIR);
+//		chThdSleepMilliseconds(100);
+//	}
+}
+
 void obstacle_detection_start(void) {
 
-//	proximity_start();
+	proximity_start();
 	VL53L0X_start();
 
-	static THD_WORKING_AREA(obstacle_detection_thd_wa, 1024);
-	obstacleThd = chThdCreateStatic(obstacle_detection_thd_wa,
-			sizeof(obstacle_detection_thd_wa),
-			NORMALPRIO, obstacle_detection_thd, NULL);
+	static THD_WORKING_AREA(TOFsensor_thd_wa, 1024);
+	TOFthd = chThdCreateStatic(TOFsensor_thd_wa, sizeof(TOFsensor_thd_wa),
+	NORMALPRIO, TOFsensor_thd, NULL);
+
+	static THD_WORKING_AREA(IRsensor_thd_wa, 1024);
+	IRthd = chThdCreateStatic(IRsensor_thd_wa, sizeof(IRsensor_thd_wa),
+	NORMALPRIO, IRsensor_thd, NULL);
 }
 
