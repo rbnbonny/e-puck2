dfdf6aedb7f5f7e1d4e62bcf87426d3eaf3f8bed
Robin Bonny <robin.bonny@epfl.ch>
2021-04-19 14:13:55 +0200

    Thread implementation

--- obstacle_detection.c
+++ obstacle_detection.c
@@ -1,22 +1,33 @@
+#include <ch.h>
+#include <hal.h>
+#include <chprintf.h>
+
 #include "sensors/proximity.h"
 #include "sensors/VL53L0X/VL53L0X.h"
 
 static thread_t *obstacleThd;
 
 static THD_FUNCTION(obstacle_detection_thd, arg) {
-     (void) arg;
-     chRegSetThreadName(__FUNCTION__);
+	(void) arg;
+	chRegSetThreadName(__FUNCTION__);
 
-     uint16_t test = VL53L0X_get_dist_mm();
+	chThdSleepMilliseconds(100);
+
+//	while (1) {
+//		uint16_t testTOF = VL53L0X_get_dist_mm();
+//		chprintf((BaseSequentialStream *) &SD3,"TOF Distance: \f mm", testTOF);
+//		chThdSleepMilliseconds(100);
+//	}
 }
 
 void obstacle_detection_start(void) {
 
-	proximity_start();
-	VL53L0X_start();
+//	proximity_start();
+//	VL53L0X_start();
 
 	static THD_WORKING_AREA(obstacle_detection_thd_wa, 1024);
-	obstalceThd = chThdCreateStatic(obstacle_detection_thd_wa, sizeof(obstacle_detection_thd_wa),
+	obstacleThd = chThdCreateStatic(obstacle_detection_thd_wa,
+			sizeof(obstacle_detection_thd_wa),
 			NORMALPRIO, obstacle_detection_thd, NULL);
 }
 
