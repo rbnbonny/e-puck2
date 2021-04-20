419c14d4b30ffd101703585c00aab3fd8af336e3
Robin Bonny <robin.bonny@epfl.ch>
2021-04-20 09:21:43 +0200

    Merge of sensor data

--- obstacle_detection.c
+++ obstacle_detection.c
@@ -1,15 +1,23 @@
 #include <ch.h>
 #include <hal.h>
 #include <chprintf.h>
+#include <obstacle_detection.h>
 
 #include "msgbus/messagebus.h"
 #include "sensors/proximity.h"
 #include "sensors/VL53L0X/VL53L0X.h"
 
-#define IR_SENSORNUM 0
+#define IR_SENSORNUM_R 3
+#define IR_SENSORNUM_RF 2
+#define IR_SENSORNUM_L 6
+#define IR_SENSORNUM_LF 7
+#define TOF_SAMPLING_WAIT 100
+//#define IR_SAMPLING_WAIT 100
 
 //static thread_t *TOFthd;
 //static thread_t *IRthd;
+
+TOFIR_msg_t TOFIR_values;
 
 messagebus_t bus;
 MUTEX_DECL(bus_lock);
@@ -21,10 +29,8 @@ static THD_FUNCTION(TOFsensor_thd, arg) {
 	chRegSetThreadName(__FUNCTION__);
 
 	while (1) {
-		volatile uint16_t testTOF = VL53L0X_get_dist_mm();
-		chprintf((BaseSequentialStream *) &SD3, "TOF Distance: %d mm \r\n",
-				testTOF);
-		chThdSleepMilliseconds(100);
+		TOFIR_values.TOF_dist = VL53L0X_get_dist_mm();
+		chThdSleepMilliseconds(TOF_SAMPLING_WAIT);
 	}
 }
 
@@ -41,17 +47,28 @@ static THD_FUNCTION(IRsensor_thd, arg) {
 	while (1) {
 		messagebus_topic_wait(proximity_topic, &prox_values,
 				sizeof(prox_values));
-		volatile int testIR = get_calibrated_prox(IR_SENSORNUM);
-		chprintf((BaseSequentialStream *) &SD3, "IR Level: %d \r\n", testIR);
-		chThdSleepMilliseconds(100);
+		TOFIR_values.IR_r_prox = get_calibrated_prox(IR_SENSORNUM_R);
+		TOFIR_values.IR_rf_prox = get_calibrated_prox(IR_SENSORNUM_RF);
+		TOFIR_values.IR_l_prox = get_calibrated_prox(IR_SENSORNUM_L);
+		TOFIR_values.IR_lf_prox = get_calibrated_prox(IR_SENSORNUM_LF);
+//		chThdSleepMilliseconds(IR_SAMPLING_WAIT);
 	}
 }
 
 static THD_WORKING_AREA(obstacle_detec_thd_wa, 2014);
-static THD_FUNCTION(obstacle_detec_thd, arg){
+static THD_FUNCTION(obstacle_detec_thd, arg) {
 	(void) arg;
 	chRegSetThreadName(__FUNCTION__);
 
+	while (1) {
+		chprintf((BaseSequentialStream *) &SD3, "TOF Distance: %d mm \r\n",
+				TOFIR_values.TOF_dist);
+		chprintf((BaseSequentialStream *) &SD3,
+				"IR Levels: R%d RF%d L%d LF%d \r\n", TOFIR_values.IR_r_prox,
+				TOFIR_values.IR_rf_prox, TOFIR_values.IR_l_prox,
+				TOFIR_values.IR_lf_prox);
+		chThdSleepMilliseconds(500);
+	}
 
 }
 
--- obstacle_detection.h
+++ obstacle_detection.h
@@ -5,6 +5,21 @@
 extern "C" {
 #endif
 
+// IR Sensor IDs
+#define IR_SENSORNUM_R 3
+#define IR_SENSORNUM_RF 2
+#define IR_SENSORNUM_L 6
+#define IR_SENSORNUM_LF 7
+
+// Struct containing proximity sensor date from TOF & IR
+typedef struct {
+	uint16_t TOF_dist;	// Frontal time-of-flight distance
+	int IR_r_prox;		// Right IR3 proximity
+	int IR_rf_prox;		// Right-front IR2 proximity
+	int IR_l_prox;		// Left IR6 proximity
+	int IR_lf_prox;		// Left-front IR7 proximity
+} TOFIR_msg_t;
+
 void obstacle_detection_start(void);
 
 #ifdef __cplusplus
