526b7c40ae31a8879df6ac1840b44277a2f52777
Robin Bonny <robin.bonny@epfl.ch>
2021-04-22 10:12:35 +0200

    Revert "Merge branch 'camera' into main"
    This reverts commit a8bb9009ae2cc9821b8ad9237997eb8c558e4554, reversing
    changes made to 208e9ad8311620491c15ae01aee508c6948bce14.

--- main.c
+++ main.c
@@ -6,58 +6,50 @@
 #include "hal.h"
 #include "memory_protection.h"
 #include <usbcfg.h>
-#include <main.h>
-#include <camera/po8030.h>
 #include <chprintf.h>
 
-#include <process_image.h>
+#include <main.h>
+#include <obstacle_detection.h>
+#include <motors.h>
 
-void SendUint8ToComputer(uint8_t* data, uint16_t size)
-{
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)"START", 5);
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)&size, sizeof(uint16_t));
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)data, size);
-}
+#define MOTORSPEED 200
 
-static void serial_start(void)
-{
-	static SerialConfig ser_cfg = {
-	    115200,
-	    0,
-	    0,
-	    0,
-	};
+static void serial_start(void) {
+	static SerialConfig ser_cfg = { 115200, 0, 0, 0, };
 
 	sdStart(&SD3, &ser_cfg); // UART3.
 }
 
-int main(void)
-{
+int main(void) {
 
-    halInit();
-    chSysInit();
-    mpu_init();
+	halInit();
+	chSysInit();
+	motors_init();
 
-    //starts the serial communication
-    serial_start();
-    //starts the USB communication
-    usb_start();
-    //starts the camera
-    dcmi_start();
-	po8030_start();
+	//starts the serial communication
+	serial_start();
+	//starts the USB communication
+	usb_start();
 
-    process_image_start();
+	obstacle_detection_start();
+	left_motor_set_speed(MOTORSPEED);
+	right_motor_set_speed(MOTORSPEED);
 
-    while(1){
-//    	chprintf((BaseSequentialStream *)&SDU1, "Test\r\n");
-    	chThdSleepSeconds(1);
-    }
+	while (1) {
+		/*
+		 * 1. Read out sensors
+		 * 2. Calculate action
+		 * 3. Actuate motors
+		 * 4. Calculate map
+		 * 5. Transmit map
+		 */
+		chThdSleepMilliseconds(1000);
+	}
 }
 
 #define STACK_CHK_GUARD 0xe2dee396
 uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
 
-void __stack_chk_fail(void)
-{
-    chSysHalt("Stack smashing detected");
+void __stack_chk_fail(void) {
+	chSysHalt("Stack smashing detected");
 }
--- makefile
+++ makefile
@@ -10,7 +10,7 @@ GLOBAL_PATH = ../../lib/e-puck2_main-processor
 
 #Source files to include
 CSRC += ./main.c \
-		./process_image.c \
+		./obstacle_detection.c \
 
 
 #Header folders to include
