9b2beaf6be609f446bbd721336d38c6961668099
Robin Bonny <robin.bonny@epfl.ch>
2021-04-27 11:17:28 +0200

    Main testing

--- main.c
+++ main.c
@@ -13,6 +13,9 @@
 #include "obstacle_detection.h"
 #include "motor_control.h"
 #include "regulator.h"
+#include "process_image.h"
+
+#define IR_THRESHOLD 500
 
 static void serial_start(void) {
 	static SerialConfig ser_cfg = { 115200, 0, 0, 0, };
@@ -45,12 +48,43 @@ int main(void) {
 		 * 4. Calculate map
 		 * 5. Transmit map
 		 */
-		chprintf((BaseSequentialStream *) &SD3, "Waiting\r\n");
-		frontal_obstacle_wait();
-		chprintf((BaseSequentialStream *) &SD3, "SP Reset\r\n");
-		palClearPad(GPIOD, GPIOD_LED5);
-		chThdSleepMilliseconds(100);
-		palSetPad(GPIOD, GPIOD_LED5);
+//		frontal_obstacle_wait();
+//		switch (get_barcode_number()) {
+//			case 0:
+//				break;
+//			case 1:
+//				break;
+//			case 2:
+//				break;
+//			case 3:
+//				break;
+//			case 4:
+//				break;
+//			case 5:
+//				break;
+//			case 6:
+//				break;
+//			case 7:
+//				break;
+//			case 8:
+//				break;
+//			case 9:
+//				break;
+//			default:
+//				break;
+//		}
+		chprintf((BaseSequentialStream *) &SD3, "LEFT IR %d   RIGHT IR %d",
+				get_TOFIR_values().IR_l_prox, get_TOFIR_values().IR_r_prox);
+
+//		if (get_TOFIR_values().IR_l_prox < IR_THRESHOLD) {
+//		chSysLock();
+//		chSysUnlock();
+		motor_turn(LEFT, 90);
+//		}
+//		if (get_TOFIR_values().IR_r_prox < IR_THRESHOLD) {
+//			motor_turn(RIGHT, 90);
+//		}
+		chThdSleepMilliseconds(1000);
 
 	}
 }
